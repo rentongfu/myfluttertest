496aad7bf8d31cee0f1b82da7122e6503fedc28b
tongfu <861454910@qq.com>
2020-07-09 17:29:22 +0800

    字体调整

--- lib/cupertino/CupertinoTabTestPage.dart
+++ lib/cupertino/CupertinoTabTestPage.dart
@@ -8,15 +8,15 @@ class CupertinoTabTestPage extends StatelessWidget{
 
     return CupertinoTabScaffold(tabBar: CupertinoTabBar(
       items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Text("First")),
-        BottomNavigationBarItem(icon: Text("Second")),
-        BottomNavigationBarItem(icon: Text("Third")),
-        BottomNavigationBarItem(icon: Text("Fourth")),
-        BottomNavigationBarItem(icon: Text("Fifth")),
+        BottomNavigationBarItem(icon: Text("First", style: TextStyle(fontSize: 14),)),
+        BottomNavigationBarItem(icon: Text("Second", style: TextStyle(fontSize: 14),)),
+        BottomNavigationBarItem(icon: Text("Third", style: TextStyle(fontSize: 14),)),
+        BottomNavigationBarItem(icon: Text("Fourth", style: TextStyle(fontSize: 14),)),
+        BottomNavigationBarItem(icon: Text("Fifth", style: TextStyle(fontSize: 14),)),
       ],
     ), tabBuilder: (context , index){
       return Center(
-        child: Text("This is Tab${index}"),
+        child: Text("This is Tab${index}", style: TextStyle(fontSize: 16),),
       );
     });
   }
